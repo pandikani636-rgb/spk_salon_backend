68295a2af62a4a653d11ccb65f27c7f6559c56eb
pandikani <pandikani636@gmail.com>
2026-09-03 00:07:37 +0530

    kmdknkd

--- src/controllers/salonController.js
+++ src/controllers/salonController.js
@@ -13,7 +13,7 @@ export const getSalons = async (req, res, next) => {
         }
       : {};
 
-    const salons = await Salon.find({ ...keyword, isActive: true, status: 'approved' });
+    const salons = await Salon.find({ ...keyword });
     res.json({
       success: true,
       data: salons,
@@ -25,14 +25,14 @@ export const getSalons = async (req, res, next) => {
 
 export const getSalonById = async (req, res, next) => {
   try {
-    const salon = await Salon.findOne({ _id: req.params.id, isActive: true, status: 'approved' });
+    const salon = await Salon.findOne({ _id: req.params.id });
     if (!salon) {
       res.status(404);
       throw new Error('Salon not found');
     }
 
-    const services = await Service.find({ salon: salon._id, isActive: true });
-    const staff = await StaffProfile.find({ salon: salon._id, isActive: true }).populate('user', 'firstName lastName avatar');
+    const services = await Service.find({ salon: salon._id });
+    const staff = await StaffProfile.find({ salon: salon._id }).populate('user', 'firstName lastName avatar');
 
     res.json({
       success: true,
